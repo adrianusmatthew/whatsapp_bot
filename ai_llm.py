b301d4044889d2e49981a5602c044a57689c63ed
adrianusmatthew <adrianus_matthew@yahoo.com>
2024-07-01 01:58:08 +0700

    feat: implement class LanguageModel

--- ai_llm.py
+++ ai_llm.py
@@ -5,38 +5,35 @@ from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 
-# TODO: implement store with DB functionality
-store = {}
-llm_model = ChatOpenAI(model="gpt-3.5-turbo")
 
+class LanguageModel:
+    def __init__(self, model_name="gpt-4o-2024-05-13"):
+        # TODO: save to DB or some shit
+        self.store = {}
+        self.llm_model = ChatOpenAI(model=model_name)
+        self.with_message_history = RunnableWithMessageHistory(
+            self.llm_model,
+            self.get_session_history
+        )
 
-def get_session_history(session_id: str) -> BaseChatMessageHistory:
-    if session_id not in store:
-        store[session_id] = ChatMessageHistory()
+    @staticmethod
+    def create_session_id(contact: str) -> str:
+        today = datetime.today().strftime('%Y-%m-%d')
+        return f"{contact}-{today}"
 
-    return store[session_id]
+    def get_session_history(self, session_id: str) -> BaseChatMessageHistory:
+        if session_id not in self.store:
+            self.store[session_id] = ChatMessageHistory()
+        return self.store[session_id]
 
-
-with_message_history = RunnableWithMessageHistory(
-    llm_model,
-    get_session_history
-)
-
-
-def create_session_id(contact: str) -> str:
-    today = datetime.today().strftime('%Y-%m-%d')
-    return f"{contact}-{today}"
-
-
-def get_llm_response(prompt: str, session_id: str) -> str:
-    config = {
-        "configurable": {
-            "session_id": session_id
+    def get_llm_response(self, prompt: str, session_id: str) -> str:
+        config = {
+            "configurable": {
+                "session_id": session_id
+            }
         }
-    }
-    response = with_message_history.invoke(
-        [HumanMessage(content=prompt)],
-        config=config,
-    )
-
-    return response.content
+        response = self.with_message_history.invoke(
+            [HumanMessage(content=prompt)],
+            config=config,
+        )
+        return response.content
