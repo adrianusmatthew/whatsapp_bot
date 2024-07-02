52704ab4cb864721b46665dcf3de354e8fb1cfa9
adrianusmatthew <adrianus_matthew@yahoo.com>
2024-07-02 09:50:47 +0700

    feat: switch to gpt-4o

--- ai_llm.py
+++ ai_llm.py
@@ -9,7 +9,7 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 class LanguageModel:
     # TODO: Implement error handling
-    def __init__(self, model_name="gpt-3.5-turbo"):
+    def __init__(self, model_name="gpt-4o"):
         # TODO: save to DB or some shit
         self.store = {}
 
